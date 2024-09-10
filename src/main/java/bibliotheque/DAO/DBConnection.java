745212f13fdeb83e14bb7cad55e5f447a3f4dbf2
radia idelkadi <idelkadiradia@gmail.com>
2024-09-10 20:58:13 +0100

    Implémentation de la connexion à la base de données avec le pattern singleton.

--- src/main/java/bibliotheque/DAO/DBConnection.java
+++ src/main/java/bibliotheque/DAO/DBConnection.java
@@ -1,6 +1,56 @@
 package main.java.bibliotheque.DAO;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DBConnection {
 
+    private static DBConnection instance;
+    private Connection connection;
 
+    private DBConnection() throws SQLException, IOException {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("main/resources/application.properties")){
+            Properties props = new Properties();
+            if (input == null) {
+                throw new IOException("Fichier de configuration non trouvé.");
+            }
+            props.load(input);
+
+            String url = props.getProperty("db.url");
+            String username = props.getProperty("db.username");
+            String password = props.getProperty("db.password");
+
+            this.connection = DriverManager.getConnection(url, username, password);
+
+        } catch (SQLException ex) {
+            throw  new SQLException("Échec de la création de la connexion à la base de données : " + ex.getMessage());
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static DBConnection getInstance() throws SQLException, IOException {
+        if (instance == null) {
+            instance = new DBConnection();
+        } else if (instance.getConnection().isClosed()) {
+            instance = new DBConnection();
+        }
+        return instance;
+    }
+
+    public void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                System.out.println("Échec de la fermeture de la connexion : " + ex.getMessage());
+            }
+        }
+    }
 }
--- src/main/java/bibliotheque/presentation/ConsoleUI.java
+++ src/main/java/bibliotheque/presentation/ConsoleUI.java
@@ -7,5 +7,13 @@ import java.sql.SQLException;
 import java.io.IOException;
 
 public class ConsoleUI {
-
+    public static void main(String[] args) {
+        try (Connection connection = DBConnection.getInstance().getConnection()) {
+            System.out.println("Connexion à la base de données réussie! Braaaavo!!");
+        } catch (SQLException ex) {
+            System.err.println("Erreur de connexion à la base de données : " + ex.getMessage());
+        } catch (IOException ex) {
+            System.err.println("Erreur lors du chargement des propriétés : " + ex.getMessage());
+        }
+    }
 }
